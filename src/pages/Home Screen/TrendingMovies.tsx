3c6d7928b4e221740849212698ae248f070e481a
AkmalRegov <m.akmal@regovtech.com>
2023-04-03 10:29:53 +0800

    Add MIT license, React Router DOM and Styled Components dependencies, and create TrendingMovies component to fetch and display data in the home screen

---
+++ src/pages/Home Screen/TrendingMovies.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+export default function TrendingMovies() {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setTrendingMovies(data);
+        console.log();
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>Trending Movies</div>
+    </>
+  );
+}
