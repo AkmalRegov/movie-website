eab0192eccffaf21b0e0ff8520953307a9610be8
AkmalRegov <m.akmal@regovtech.com>
2023-04-12 10:30:27 +0800

    Tried to implement half-rating logic but didn't work, for now only full 1 whole rating logic is implemented.

--- src/component/DynamicRatings.tsx
+++ src/component/DynamicRatings.tsx
@@ -1,14 +1,42 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 
 export const DynamicRatings: React.FC = () => {
   const [activeItems, setActiveItems] = useState(-1);
+  const [hoverActiveItems, setHoverActiveItems] = useState(-1);
+  const [isHovered, setIsHovered] = useState(false);
+  const ratingContainerRef = useRef() as React.MutableRefObject<HTMLDivElement>;
   const totalCount = [1, 2, 3, 4, 5];
+  const precision = 1;
 
-  const handleClick = (index: number) => {
-    setActiveItems(index);
-    console.log(`activeItems is: ${activeItems}`);
-  };
+  //   const handleClick = (index: number) => {
+  //     setActiveItems(index);
+  //     console.log(`activeItems is: ${activeItems}`);
+  //   };
+
+  function calculateRating(e: React.MouseEvent) {
+    const { width, left } = ratingContainerRef.current.getBoundingClientRect();
+    let percent = (e.clientX - left) / width;
+    const numberInItems = percent * totalCount.length;
+    const nearestNumber = Math.round((numberInItems + precision / 2) / precision) * precision;
+    return Number(nearestNumber.toFixed(precision.toString().split(".")[1]?.length || 0));
+  }
+
+  function handleClick(e: React.MouseEvent) {
+    setIsHovered(false);
+    setActiveItems(calculateRating(e));
+  }
+
+  function handleMouseMove(e: React.MouseEvent) {
+    setIsHovered(true);
+    setHoverActiveItems(calculateRating(e));
+  }
+
+  function handleMouseLeave(e: React.MouseEvent) {
+    setHoverActiveItems(-1);
+    setIsHovered(false);
+  }
+
   return (
     <>
       <div style={{ display: "flex", alignItems: "center", marginTop: "10px", gap: "0.5rem" }}>
@@ -19,8 +47,26 @@ export const DynamicRatings: React.FC = () => {
             cursor: "pointer",
             textAlign: "left",
           }}
+          ref={ratingContainerRef}
+          onClick={handleClick}
+          onMouseMove={handleMouseMove}
+          onMouseLeave={handleMouseLeave}
         >
           {totalCount.map((arr, index) => {
+            //half-rating logic doesn't work...
+            const activeState = isHovered ? hoverActiveItems : activeItems;
+
+            const showEmptyIcon = activeState === -1 || activeState < index + 1;
+
+            const isActiveRating = activeState !== 1;
+            const isRatingWithPrecision = activeState % 1 !== 0;
+            const isRatingEqualToIndex = Math.ceil(activeState) === index + 1;
+            const showRatingWithPrecision =
+              isActiveRating && isRatingWithPrecision && isRatingEqualToIndex;
+            // const showRatingWithPrecision = true;
+            // console.log(`showRatingWithPrecision is: ${showRatingWithPrecision}`);
+            // console.log(`${(activeState % 1) * 100}`);
+
             return (
               <div
                 key={index}
@@ -28,13 +74,10 @@ export const DynamicRatings: React.FC = () => {
                   position: "relative",
                   cursor: "pointer",
                 }}
-                onClick={() => handleClick(index)}
               >
                 <div
                   style={{
-                    display: "flex",
-                    maxWidth: "fit-content",
-                    width: index <= activeItems ? "100%" : "0%",
+                    width: showRatingWithPrecision ? `${(activeState % 1) * 100}%` : "0%",
                     overflow: "hidden",
                     position: "absolute",
                   }}
@@ -42,9 +85,12 @@ export const DynamicRatings: React.FC = () => {
                   <AiFillStar size={20} />
                 </div>
                 <div
-                  style={{ display: "flex", width: "fit-content", position: "relative", bottom: 0 }}
+                  style={{
+                    color: showEmptyIcon ? "gray" : "inherit",
+                  }}
                 >
-                  <AiOutlineStar size={20} />
+                  {/* <AiOutlineStar size={20} /> */}
+                  {showEmptyIcon ? <AiOutlineStar size={20} /> : <AiFillStar size={20} />}
                 </div>
               </div>
             );
