3229e952e801f386efb4cefb194d5f50cf9a999d
AkmalRegov <m.akmal@regovtech.com>
2023-04-13 13:43:41 +0800

    Refactor IconDiv to use styled-components for better readability and maintainability

--- src/pages/MoviesDetails/IconDiv.tsx
+++ src/pages/MoviesDetails/IconDiv.tsx
@@ -2,59 +2,67 @@ import React from "react";
 import { oneMovieData } from "./MoviesDetails";
 import UserScoreFC from "./UserScoreFC";
 import { IoHeartCircle, IoListCircleSharp } from "react-icons/io5";
-import { BsFillBookmarkPlusFill } from "react-icons/bs";
+import { BsFillBookmarkFill } from "react-icons/bs";
 import { MdStars } from "react-icons/md";
+import styled from "styled-components";
+
+const SWrapperDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+const SIconWrapperDiv = styled.div`
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SOuterWhiteCircleSpan = styled.span`
+  display: flex;
+  background-color: white;
+  justify-content: center;
+  align-items: center;
+  width: 38px;
+  height: 38px;
+  border: 1px solid white;
+  border-radius: 50%;
+`;
+
+const SInnerBlackCircleSpan = styled.span`
+  display: flex;
+  background-color: black;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  border: 1px solid white;
+  border-radius: 50%;
+`;
+
+const IconStyle = { backgroundColor: "white", borderRadius: "100%" };
 
 const IconDiv: React.FC<{ movieData: oneMovieData }> = ({ movieData }) => {
   return (
     <>
-      <div
-        style={{
-          marginTop: "20px",
-          marginBottom: "20px",
-          display: "flex",
-          alignItems: "center",
-          gap: "1rem",
-        }}
-      >
+      <SWrapperDiv>
         <div>
           <UserScoreFC movieData={movieData} />
         </div>
-        <div style={{ display: "flex", gap: "1rem" }}>
-          <IoListCircleSharp
-            size={40}
-            color="black"
-            style={{
-              backgroundColor: "white",
-              borderRadius: "100%",
-            }}
-          />
-          <IoHeartCircle
-            size={40}
-            color="black"
-            style={{
-              backgroundColor: "white",
-              borderRadius: "100%",
-            }}
-          />
-          <BsFillBookmarkPlusFill
-            size={40}
-            color="black"
-            style={{
-              backgroundColor: "white",
-              borderRadius: "100%",
-            }}
-          />
-          <MdStars
-            size={40}
-            color="black"
-            style={{
-              backgroundColor: "white",
-              borderRadius: "100%",
-            }}
-          />
-        </div>
-      </div>
+        <SIconWrapperDiv>
+          <IoListCircleSharp size={40} color="black" style={IconStyle} />
+          <IoHeartCircle size={40} color="black" style={IconStyle} />
+          <SOuterWhiteCircleSpan>
+            <SInnerBlackCircleSpan>
+              <BsFillBookmarkFill size={16} color="white" />
+            </SInnerBlackCircleSpan>
+          </SOuterWhiteCircleSpan>
+          <MdStars size={38} color="black" style={IconStyle} />
+        </SIconWrapperDiv>
+      </SWrapperDiv>
     </>
   );
 };
